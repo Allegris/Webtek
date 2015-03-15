16131987892345210fc795e8b8c774fb2e24b827
Camilla Grevy <CamillaGrevy@hotmail.com>
2015-03-15 18:22:37 +0100

    Forvirring
    LAvet noget om.. Ved ikke hvad.. Forvirrende...

--- src/dk/cs/dwebtek/ShopService.java
+++ src/dk/cs/dwebtek/ShopService.java
@@ -1,7 +1,9 @@
 package dk.cs.dwebtek;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -28,6 +30,8 @@ public class ShopService {
     /**
      * Our Servlet session. We will need this for the shopping basket
      */
+	private static final String shopKey = "C0CF3F21E1B721284D08A9AE";
+	
     HttpSession session;
     
     ArrayList<Item> items;
@@ -119,8 +123,56 @@ public class ShopService {
     }
     
     @GET
-    @Path("createCustomer")
-    public String makeCustomer(){
+    @Path("/createCustomer")
+    public String makeCustomer(String JSON){
+    	System.out.println(JSON);
+    	JSONObject customer = new JSONObject(JSON);
+    	String username = customer.getString("username");
+    	String password = customer.getString("password");
+    	Document doc = generateCreateCustomer(username, password);
+    	
+    	HttpURLConnection con;
+		try {
+			con = (HttpURLConnection) new URL(
+					"http://services.brics.dk/java4/cloud/createCustomer")
+					.openConnection();
+		
+			con.setDoOutput(true);
+			BufferedOutputStream out = new BufferedOutputStream(
+				con.getOutputStream());
+			PrintStream pout = new PrintStream(out);
+			XMLOutputter xmlOutPutter = new XMLOutputter();
+			pout.append(xmlOutPutter.outputString(doc));
+			pout.flush();    	
+			System.out.println(xmlOutPutter.outputString(doc));
+			int response = con.getResponseCode();
+
+			if (response != 200) {
+				System.out
+					.println("Something went wrong while creating the customer: "
+							+ response);
+			} else {
+				System.out.println("The customer was succesfully made");
+			}
+			SAXBuilder builder = new SAXBuilder();
+			InputStream in = con.getInputStream();
+			//Making XML
+			Document responseDoc = builder.build(in);
+			System.out.println(xmlOutPutter.outputString(responseDoc));
+			
+		
+		}catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+    	
+    	
 //    	step 0 parse the incoming JSON to get the user name and password
 //    	step 1 use JDOM to create the createCustomer XML and print it to console
 //    	step 2 send the XML to the server and print out the reply
@@ -128,6 +180,31 @@ public class ShopService {
 //    	step 4 return the JSON
     	return "ok";
     }
+    
+    public Document generateCreateCustomer(String username, String password) {
+		// Creating a new xml program to hold the createCustomer data
+		Document doc = new Document();
+
+		// Adds a new element called createCustomer and sets it as RootElement
+		Element createCustomer = new Element("createCustomer", ns);
+		doc.setRootElement(createCustomer);
+
+		// Copies the customerName from the source document
+		Element customerName = new Element("customerName", ns).setText(username);
+		
+		// Copies the customerPass from the source document
+		Element customerPass = new Element("customerPass", ns).setText(password);
+		
+		// Copies the shopkey from the source document
+		Element elshopKey = new Element("shopKey", ns).setText(shopKey);
+		
+		//adds shopKey, customerName and customerPass as children																
+		createCustomer.addContent(customerName);
+		createCustomer.addContent(customerPass);
+		createCustomer.addContent(elshopKey);
+		
+		return doc;
+	}
 }
 
 
