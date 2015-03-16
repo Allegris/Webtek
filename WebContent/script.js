0f2fcdabdac9385058ce089d413a9b104ee1b23e
Camilla Grevy <CamillaGrevy@hotmail.com>
2015-03-16 22:00:15 +0100

    FÆRDIG!!
    Vi er super gode! :D

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,7 +1,11 @@
+var customerID =-1;
+var arrayOfItems = [];
+var totalPrice = 0;
+
 //Run this function when we have loaded the HTML document
 window.onload = function () {
     //This code is called when the body element has been loaded and the application starts
-
+	
     //Request items from the server. The server expects no request body, so we set it to null
     sendRequest("GET", "rest/shop/items", null, function (itemsText) {
         //This code is called when the server has sent its data
@@ -10,15 +14,16 @@ window.onload = function () {
     });
 
     //Register an event listener for button clicks
-    var updateButton = document.getElementById("update");
-    addEventListener(updateButton, "click", function () {
+    function updateList() {
         //Same as above, get the items from the server
         sendRequest("GET", "rest/shop/items", null, function (itemsText) {
             //This code is called when the server has sent its data
             var items = JSON.parse(itemsText);
             addItemsToTable(items);
         });
-    });
+    }
+    
+   
     
     //Register an event listener for createButton clicks
     var createButton = document.getElementById("create");
@@ -29,13 +34,62 @@ window.onload = function () {
     	userRequest.username = username;
     	userRequest.password = password;
     	var stringOfJSON = JSON.stringify(userRequest);
-    	sendRequest("GET", "rest/shop/createCustomer", stringOfJSON, function (itemsText) {
+    	sendRequest("POST", "rest/shop/createCustomer", stringOfJSON, function (response) {
             //This code is called when the server has sent its data
-            var items = JSON.parse(itemsText);
-            addItemsToTable(items);
-        sendRequest(httpMethod, url, body, responseHandler)
+    	    var responseDiv = document.getElementById("response");
+    	    responseDiv.innerHTML = response;
         });
     });
+    
+    var loginButton = document.getElementById("login");
+    addEventListener(loginButton, "click", function () {
+    	var username = document.getElementById("username").value;
+    	var password = document.getElementById("password").value;
+    	var userRequest = {};
+    	userRequest.username = username;
+    	userRequest.password = password;
+    	var stringOfJSON = JSON.stringify(userRequest);
+    	sendRequest("POST", "rest/shop/login", stringOfJSON, function (response) {
+            //This code is called when the server has sent its data
+    	    var responseDiv = document.getElementById("response");
+    	    if (response == "Error"){
+    	    	responseDiv.innerHTML = "Error";
+    	    }else{
+    	    	customerID = response;
+    	    	responseDiv.innerHTML = response;
+    	    }
+    	    
+        });
+    });
+    
+    
+    var buyButton = document.getElementById("buy");
+    addEventListener(buyButton, "click", function () {
+   
+    	var JSONObj = {customerID: customerID, items: arrayOfItems};
+    	var stringOfJSON = JSON.stringify(JSONObj);
+    	sendRequest("POST", "rest/shop/sellItems", stringOfJSON, function (response) {
+            //This code is called when the server has sent its data
+    	    var responseDiv = document.getElementById("buyFeedback");
+    	    if (response == "Success"){
+    	    	responseDiv.innerHTML = "Your items were bought!";
+    	    	totalPrice = 0;
+    	    	arrayOfItems = [];
+    	    	
+    	    	var cartContent = document.getElementById("cartContent");
+    			cartContent.innerHTML = "";
+    			var total = document.getElementById("totalPrice");
+    			total.innerHTML = totalPrice;
+    			updateList();
+    	    }else{
+    	    	responseDiv.innerHTML = "Something went wrong. Make sure you are logged in!";
+
+    	   
+    	    }
+    	    
+        });
+    });
+    
 };
 
 function addItemsToTable(items) {
@@ -59,24 +113,45 @@ function addItemsToTable(items) {
         tr.appendChild(priceCell);
         
         var descriptionCell = document.createElement("td");
-        descriptionCell.textContent = item.description;
+        descriptionCell.innerHTML = item.description;
         tr.appendChild(descriptionCell);
         
         var stockCell = document.createElement("td");
         stockCell.textContent = item.stock;
         tr.appendChild(stockCell);
-        
-        var inCartCell = document.createElement("td");
-        inCartCell.textContent = item.inCart;
-        tr.appendChild(inCartCell);
+                
+        var buttonCell = document.createElement("td");
+        var button = document.createElement("button");
+        addEventListener(button, "click", (function(id, price, stock, name) {
+            return function() {
+                addToCart(id, price, stock, name);
+             };
+         })(item.id, item.price, item.stock, item.name));
+        button.innerHTML = "Add to cart";
+        buttonCell.appendChild(button);
+        tr.appendChild(buttonCell);
         
         var idCell = document.createElement("td");
         idCell.textContent = item.id;
         tr.appendChild(idCell);
 
+        
         tableBody.appendChild(tr);
     }
 }
+
+
+function addToCart(id, price, stock, name){
+	if (stock > 0){
+		var cartContent = document.getElementById("cartContent");
+		cartContent.innerHTML = cartContent.innerHTML + name + ", ";
+		arrayOfItems.push(id);
+		totalPrice += price;
+		var total = document.getElementById("totalPrice");
+		total.innerHTML = totalPrice;
+	}
+	
+}	
 
 
 /////////////////////////////////////////////////////
@@ -107,11 +182,12 @@ function sendRequest(httpMethod, url, body, responseHandler) {
     if (httpMethod == "POST") {
         http.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
     }
+    http.send("json="+body);
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
             responseHandler(http.responseText);
         }
     };
-    http.send(body);
+    
 }
 
--- src/dk/cs/dwebtek/ShopService.java
+++ src/dk/cs/dwebtek/ShopService.java
@@ -18,12 +18,17 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.XML;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 @Path("shop")
 public class ShopService {
@@ -122,9 +127,68 @@ public class ShopService {
     	}
     }
     
-    @GET
+    @POST
+    @Path("/login")
+    public String login(@FormParam("json") String JSON){
+    	System.out.println(JSON);
+    	JSONObject customer = new JSONObject(JSON);
+    	String username = customer.getString("username");
+    	String password = customer.getString("password");
+    	Document doc = generateLogin(username, password);
+    	
+    	HttpURLConnection con;
+		try {
+			con = (HttpURLConnection) new URL(
+					"http://services.brics.dk/java4/cloud/login")
+					.openConnection();
+		
+			con.setDoOutput(true);
+			BufferedOutputStream out = new BufferedOutputStream(
+				con.getOutputStream());
+			PrintStream pout = new PrintStream(out);
+			XMLOutputter xmlOutPutter = new XMLOutputter();
+			pout.append(xmlOutPutter.outputString(doc));
+			pout.flush();    	
+			System.out.println(xmlOutPutter.outputString(doc));
+			int response = con.getResponseCode();
+
+			if (response != 200) {
+				System.out
+					.println("Something went wrong while logging in: "
+							+ response);
+				return "Error";
+			} else {
+				System.out.println("The customer was succesfully logged in");
+				
+					
+			SAXBuilder builder = new SAXBuilder();
+			InputStream in = con.getInputStream();
+			//Making XML
+			Document responseDoc = builder.build(in);
+			String responseDocString = xmlOutPutter.outputString(responseDoc);
+			System.out.println(responseDocString);
+			return responseDoc.getRootElement().getChild("customerID",ns).getValue();
+			}
+		}catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		}
+		
+	
+//    	/step 0 parse the incoming JSON to get the user name and password
+//    	/step 1 use JDOM to create the createCustomer XML and print it to console
+//    	/step 2 send the XML to the server and print out the reply
+//    	/step 3 parse the reply from the server and translate it into JSON
+//    /	step 4 return the JSON
+    	return "Error";
+    }
+    
+    @POST
     @Path("/createCustomer")
-    public String makeCustomer(String JSON){
+    public String makeCustomer(@FormParam("json") String JSON){
     	System.out.println(JSON);
     	JSONObject customer = new JSONObject(JSON);
     	String username = customer.getString("username");
@@ -151,34 +215,135 @@ public class ShopService {
 				System.out
 					.println("Something went wrong while creating the customer: "
 							+ response);
+				return "Something went wrong while creating the customer :(";
 			} else {
 				System.out.println("The customer was succesfully made");
+				return "The customer was succesfully made! :D";
 			}
-			SAXBuilder builder = new SAXBuilder();
-			InputStream in = con.getInputStream();
-			//Making XML
-			Document responseDoc = builder.build(in);
-			System.out.println(xmlOutPutter.outputString(responseDoc));
-			
+//			SAXBuilder builder = new SAXBuilder();
+//			InputStream in = con.getInputStream();
+//			//Making XML
+//			Document responseDoc = builder.build(in);
+//			String responseDocString = xmlOutPutter.outputString(responseDoc);
+//			System.out.println(responseDocString);
+//			JSONObject JSONResponse = XML.toJSONObject(responseDocString);
+//			return JSONResponse.toString();
 		
 		}catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		}// catch (JDOMException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
+	
+//    	/step 0 parse the incoming JSON to get the user name and password
+//    	/step 1 use JDOM to create the createCustomer XML and print it to console
+//    	/step 2 send the XML to the server and print out the reply
+//    	/step 3 parse the reply from the server and translate it into JSON
+//    /	step 4 return the JSON
+    	return "error";
+    }
+    
+    @POST
+    @Path("/sellItems")
+    public String sell(@FormParam("json") String JSON){
+    	System.out.println(JSON);
+    	JSONObject JSONOb = new JSONObject(JSON);
+    	JSONArray JSONItems = JSONOb.getJSONArray("items");
+    	String customerID = JSONOb.getInt("customerID")+"";
+    	
+    	for(int i = 0; i < JSONItems.length(); i++){
+
+    		int itemID = JSONItems.getInt(i);
+    		
+    		Document doc = generateSellItem(itemID, customerID);
+
+    		HttpURLConnection con;
+    		try {
+    			con = (HttpURLConnection) new URL(
+    					"http://services.brics.dk/java4/cloud/sellItems")
+    			.openConnection();
+
+    			con.setDoOutput(true);
+    			BufferedOutputStream out = new BufferedOutputStream(
+    					con.getOutputStream());
+    			PrintStream pout = new PrintStream(out);
+    			XMLOutputter xmlOutPutter = new XMLOutputter();
+    			pout.append(xmlOutPutter.outputString(doc));
+    			pout.flush();    	
+    			System.out.println(xmlOutPutter.outputString(doc));
+    			int response = con.getResponseCode();
+
+    			if (response != 200) {
+    				System.out
+    				.println("Something went wrong while buying the items: "
+    						+ response);
+    				return "Error";
+    			} else {
+    				System.out.println("The items were bought");
+    				return "Success";
+
+    				
+    			}
+    		}catch (MalformedURLException e) {
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+    	}
+    	return "Error";
+    }
+    
+    public Document generateSellItem(int itemID, String customerID) {
+		// Creating a new xml program to hold the createCustomer data
+		Document doc = new Document();
+
+		// Adds a new element called sellItem and sets it as RootElement
+		Element sellItems = new Element("sellItems", ns);
+		doc.setRootElement(sellItems);
+
+		// Copies the ItemIDEl from the source document
+		Element itemIDEl = new Element("itemID", ns).setText(itemID+"");
 		
-    	
-    	
-//    	step 0 parse the incoming JSON to get the user name and password
-//    	step 1 use JDOM to create the createCustomer XML and print it to console
-//    	step 2 send the XML to the server and print out the reply
-//    	step 3 parse the reply from the server and translate it into JSON
-//    	step 4 return the JSON
-    	return "ok";
+		// Copies the customerIDEl from the source document
+		Element customerIDEl = new Element("customerID", ns).setText(customerID);
+		
+		// Copies the saleAmount from the source document
+		Element saleAmount = new Element("saleAmount", ns).setText("1");
+		
+		// Copies the shopkey from the source document
+		Element elshopKey = new Element("shopKey", ns).setText(shopKey);
+		
+		//adds shopKey, customerName and customerPass as children																
+		sellItems.addContent(itemIDEl);
+		sellItems.addContent(customerIDEl);
+		sellItems.addContent(elshopKey);
+		sellItems.addContent(saleAmount);	
+		return doc;
+	}
+    
+    public Document generateLogin(String username, String password){
+    	// Creating a new xml program to hold the createCustomer data
+		Document doc = new Document();
+		
+		// Adds a new element called createCustomer and sets it as RootElement
+		Element login = new Element("login", ns);
+		doc.setRootElement(login);
+
+		// Copies the customerName from the source document
+		Element customerName = new Element("customerName", ns).setText(username);
+		
+		// Copies the customerPass from the source document
+		Element customerPass = new Element("customerPass", ns).setText(password);
+			
+		//adds shopKey, customerName and customerPass as children																
+		login.addContent(customerName);
+		login.addContent(customerPass);
+		
+		return doc;
     }
     
     public Document generateCreateCustomer(String username, String password) {
